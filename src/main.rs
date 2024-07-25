1436913fc6dfcc1c1bcfebc3192fbca82381a49f
fr0zty <darshn0108@gmail.com>
2024-07-25 23:03:41 +0200

    Move camera into plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod camera;
+use bevy::prelude::*;
+use camera::CameraPlugin;
 fn main() {
     println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(CameraPlugin)
+        .run();
 }
